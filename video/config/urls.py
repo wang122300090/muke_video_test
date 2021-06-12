86acb441ecd36ce354530ec20f2262f12561e95a
wangjiancheng <1561585776@qq.com>
2021-06-12 15:35:30 +0800

    这是一个初始化的版本

---
+++ video/config/urls.py
@@ -0,0 +1,26 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# coding: uft-8
+from django.contrib import admin
+from django.urls import path, include
+from app.client import urls as client_urls
+from app.dashboard import urls as dashboard_urls
+
+urlpatterns = [
+    # path('admin/', admin.site.urls), # 并不使用自带的admin,可以去掉
+    path('dashboard/', include(dashboard_urls)),
+    path('client/', include(client_urls))
+]
